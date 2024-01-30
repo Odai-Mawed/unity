9b8da2c2c7bba11d69afdc4475c1989292ddb7a8
Odai <76111555+Odai-Mawed@users.noreply.github.com>
2024-01-30 14:22:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,21 +15,22 @@ let fileCounter = 1;
 
 app.post('/upload', async (req, res) => {
   try {
-    const jsonData = JSON.parse(req.body);
+    console.log(req.body)
+//    const jsonData = JSON.parse(req.body);
 
     // Beispiel: Erstelle den Dateinamen mit einer incrementierten Zahl
-    const fileName = `jsonFile${fileCounter++}.json`;
+  //  const fileName = `jsonFile${fileCounter++}.json`;
 
     // Beispiel: Lade den JSON-String in den S3-Bucket hoch
-    await s3
-      .putObject({
-        Body: JSON.stringify(jsonData),
-        Bucket: 'sever-1-bucket-1',
-        Key: fileName
-      })
-      .promise();
+    //await s3
+     // .putObject({
+      //  Body: JSON.stringify(jsonData),
+      //  Bucket: 'sever-1-bucket-1',
+      //  Key: fileName
+      //})
+      //.promise();
 
-    console.log('Daten erfolgreich in S3 hochgeladen:', fileName);
+    //console.log('Daten erfolgreich in S3 hochgeladen:', fileName);
 
     res.send('Daten erfolgreich hochgeladen!');
   } catch (error) {
